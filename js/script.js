cf90ff3b1f7f42b61dcdadcd64d01336102d5f23
Xinyue Li <xinyuel7@illinois.edu>
2020-05-12 02:34:26 -0500

    Updates

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $('#slides').superslides({
+        animation:'fade',
+        play:3000,
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings:["Looking for 2021 Software engineer intern/Co-op", "Quicket Solution intern", "undergrad researcher at NCSA"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false,
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    $(window).scroll(function(){
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+            $('.chart').easyPieChart({
+                easing: "easeInOut",
+                barColor:"#fff",
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent))
+                }
+            });
+        }
+    });
+});
+
